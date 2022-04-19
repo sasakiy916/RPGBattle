0264d1768cd4e55ef62beb62f1f714d0b5d7bd95
sasakiy916 <sasaki3y98521j@gmail.com>
2022-04-19 18:21:14 +0900

    上級者向け

--- Main.java
+++ Main.java
@@ -1,63 +1,9 @@
 import java.util.*;
 public class Main{
 	public static void main(String[] args){
-		Random r = new Random();
-		//勇者のインスタンス
-		Hero hero = new Hero("勇者");
-		//モンスターの群れ(モンスターのインスタンス)
-		Monster[] monsters = {
-			new Monster("スライム"),
-			new Monster("ヅライム"),
-		};
-		int monstersNum = monsters.length;//モンスターの残り匹数
-		System.out.println();
-		//戦闘開始
-		showHp(hero,monsters);
-		while(true){
-			//勇者のターン
-			//どっちのモンスターを攻撃するか
-			int target = r.nextInt(2);
-			Monster monster;
-			if(monsters[target].hp > 0){
-				//HPがあれば攻撃
-				monster = monsters[target];
-			}else{
-				//targetのHPが無ければもう片方を攻撃
-				monster = monsters[Math.abs(target-1)];
-			}
-			//勇者の攻撃
-			hero.attack(monster);
-			showHp(hero,monsters);
-			//モンスターを倒したかどうか確認
-			if(monster.hp <= 0){
-				System.out.printf("%sをたおした！%n%n",monster.name);
-				//showHp(hero,monsters);
-				monstersNum--;
-				//モンスターの群れ全滅
-				if(monstersNum == 0){
-					System.out.println();
-					System.out.println("モンスターをすべてたおした！");
-					return;
-				}
-			}
-			//モンスターのターン
-			//モンスターの攻撃
-			for(int i=0;i<monsters.length;i++){
-				//モンスターが生きてれば攻撃
-				if(monsters[i].hp > 0){
-					monsters[i].attack(hero);
-					//勇者がやられたか確認
-					if(hero.hp < 0){
-						showHp(hero,monsters);
-						System.out.printf("%sはやられてしまった・・・%n",hero.name);
-						return;
-					}
-					showHp(hero,monsters);
-				}
-			}
-		}
+		//ここの処理を書く
 	}
-	//現在のHp表示メソッド
+	//現在のHp表示メソッド(ご自由に使用してください)
 	public static void showHp(Hero h,Monster[] monsters){
 		String line = "---------------";
 		System.out.println(line);
